36aa151b8a897d87ff84521e73f11abd7aae4cac
vladke <130487670+Vladke2@users.noreply.github.com>
2024-09-14 19:50:32 +0300

    Add files via upload

---
+++ authorization/endpoint.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from auth import get_password_hash, verify_password, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, SECRET_KEY, ALGORITHM
+from schemas import UserCreate
+from models import database, User
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from datetime import timedelta
+
+app = FastAPI()
+
+# database.Base.metadata.create_all(bind=database.database)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_db():
+    db = database.Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/register/")
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_password = get_password_hash(user.password)
+    db_user = User.User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"username": db_user.username}
+
+
+@app.post("/token", response_model=dict)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User.User).filter(User.User.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/")
+def read_users_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(User.User).filter(User.User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return {"username": user.username}
---
+++ authorization/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+
+
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str
+#
+#
+# class TokenData(BaseModel):
+#     ...
+#
+#
+# class UserBase(BaseModel):
+#     username: str
+#
+#
+# class UserCreate(UserBase):
+#     password: str
+#
+#
+# class User(UserBase):
+#     id: int
+#
+#     class Config:
+#         orm_mode = True
