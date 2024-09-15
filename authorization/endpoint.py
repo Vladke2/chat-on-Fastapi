0edf93466b341bfcacaefe2c7c1e2a89d9f9801a
vladke <130487670+Vladke2@users.noreply.github.com>
2024-09-15 19:10:57 +0300

    Update endpoint.py

--- authorization/endpoint.py
+++ authorization/endpoint.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from auth import get_password_hash, verify_password, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, SECRET_KEY, ALGORITHM
-from schemas import UserCreate
+from authorization.auth import get_password_hash, verify_password, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, SECRET_KEY, ALGORITHM
+from authorization.schemas import UserCreate
 from models import database, User
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
@@ -9,7 +9,8 @@ from datetime import timedelta
 
 app = FastAPI()
 
-# database.Base.metadata.create_all(bind=database.database)
+
+database.Base.metadata.create_all(bind=database.database)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -25,7 +26,7 @@ def get_db():
 @app.post("/register/")
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = get_password_hash(user.password)
-    db_user = User.User(username=user.username, hashed_password=hashed_password)
+    db_user = User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -34,14 +35,14 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
 
 @app.post("/token", response_model=dict)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User.User).filter(User.User.username == form_data.username).first()
+    user = db.query(User).filter(User.username == form_data.username).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
@@ -60,7 +61,7 @@ def read_users_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    user = db.query(User.User).filter(User.User.username == username).first()
+    user = db.query(User).filter(User.username == username).first()
     if user is None:
         raise credentials_exception
     return {"username": user.username}
